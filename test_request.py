affe1a25a6d80853d3680a31cd6f9ecda56d7e23
esjoal <estevez036@gmail.com>
2025-04-22 20:11:58 +0200

    Update test_request.py

--- test_request.py
+++ test_request.py
@@ -6,9 +6,9 @@ url = "https://team-challenge-t08-finale.onrender.com/predict"
 # Parámetros de entrada (ajusta según los valores del dataset)
 params = {
     "sepal_length": 6.1,
-    "sepal_width": 2.9,
-    "petal_length": 4.7,
-    "petal_width": 1.4
+    "sepal_width": 2.6,
+    "petal_length": 5.7,
+    "petal_width": 2.4
 }
 
 # Realizar la petición GET
